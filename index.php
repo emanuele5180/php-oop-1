8db265fc31a432c014c5b1f5a25a0faa11c97a8f
emanuele5180 <macina5180>
2020-09-22 16:40:34 +0200

    add basic css

--- index.php
+++ index.php
@@ -1,50 +1,76 @@
-<?php
+<style media="screen">
 
 
-  class User {
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    public $name;
-    public $nickname;
-    public $age;
-    public $mail;
-    public $photo;
-
-    public function __construct($name,$nickname,$age,$mail,$photo = 'photo'){
-
-      $this -> name = $name;
-      $this -> nickname = $nickname;
-      $this -> age = $age;
-      $this -> mail = $mail;
-      $this -> photo = $photo;
-    }
-
-    // public function getDetails(){
-    //   return $this -> name . '<br>'. $this -> nickname . '<br>'. $this -> age . '<br>'. $this -> mail . '<br>'. $this -> photo;
-    // }
-
-    public function __toString() {
-      return $this -> name . '<br>'.
-             $this -> nickname . '<br>'.
-             $this -> age . '<br>'.
-             $this -> mail . '<br>'.
-             $this -> photo;
-
-    }
-
+  .container {
+    background-color: lightblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    font-weight: bold;
   }
 
 
-  $user1 = new User('Pluto','pippo1',20,"pluto@gmail.com");
-  $user2 = new User('Topolino','paperino',20,"topolino@gmail.com");
+</style>
 
-  // echo $user1 -> getDetails();
+<div class="container">
 
-  echo $user1 . '<hr>';  
-  echo $user2;
+  <?php
+    class User {
+
+      public $name;
+      public $nickname;
+      public $age;
+      public $mail;
+      public $photo;
+
+      public function __construct($name,$nickname,$age,$mail,$photo = 'photo'){
+
+        $this -> name = $name;
+        $this -> nickname = $nickname;
+        $this -> age = $age;
+        $this -> mail = $mail;
+        $this -> photo = $photo;
+      }
+
+      // public function getDetails(){
+      //   return $this -> name . '<br>'. $this -> nickname . '<br>'. $this -> age . '<br>'. $this -> mail . '<br>'. $this -> photo;
+      // }
+
+      public function __toString() {
+        return $this -> name . '<br>'.
+               $this -> nickname . '<br>'.
+               $this -> age . '<br>'.
+               $this -> mail . '<br>'.
+               $this -> photo;
+
+      }
+
+    }
+
+
+    $user1 = new User('Pluto','pippo1',20,"pluto@gmail.com");
+    $user2 = new User('Topolino','paperino',20,"topolino@gmail.com");
+
+    // echo $user1 -> getDetails();
+
+    echo $user1 . '<hr>';
+    echo $user2;
+
+
+
+  ?>
 
 
 
 
-
-
-?>
+</div>
